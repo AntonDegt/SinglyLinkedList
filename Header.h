0cd8b612763a41867ac2f6a13df6a60cc9f76a61
AntonDegt <108671823+AntonDegt@users.noreply.github.com>
2022-07-04 15:05:53 +0300

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,27 @@
+#pragma once
+class SinglyLinkedList
+{
+public:
+	struct Node;
+
+private:
+	Node* head = nullptr;
+	Node* tail = nullptr;
+	int count = 0;
+
+public:
+	~SinglyLinkedList(void);
+
+	void AddToHead(int data);
+	void AddToTail(int data);
+
+	void InsertInto(int data, int position);
+	void DeleteFromHead();
+	void DeleteFromTail();
+	void DeleteByIndex(int position);
+
+	void Clear();
+	void Print() const;
+	int GetCount() const;
+	int IndexOf(int data) const;
+};
